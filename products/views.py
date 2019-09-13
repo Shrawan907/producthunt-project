6c265d69f4b16d6b813f66fd354beb1d803d64b3
Shrawan <shrawan07090@gmail.com>
2019-09-13 12:35:19 +0530

    all ok

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
 def home(request):
+   # it will get all the jobobjects from db and turn them to
+                            # python objects and then u can use inside of the code
     return render(request, 'products/home.html')
